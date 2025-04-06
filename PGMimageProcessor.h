6e2a864f04eaf15fd68380b7fb094ae05a89b7c5
Maphuti <Fabridgeshilabje01@gmail.com>
2025-04-06 22:46:01 +0200

    Added the PGMimageProcessor header class with all required methods in the assignment specs

---
+++ PGMimageProcessor.h
@@ -0,0 +1,34 @@
+#ifndef PGM_IMAGE_PROCESSOR_H
+#define PGM_IMAGE_PROCESSOR_H
+
+#include <vector>
+#include <string>
+#include "ConnectedComponent.h"
+#include "PGMimage.h"
+
+class PGMimageProcessor {
+private:
+    PGMimage image;
+    std::vector<std::unique_ptr<ConnectedComponent>> components;
+
+    
+public:
+    explicit PGMimageProcessor(const std::string& filename);
+    ~PGMimageProcessor() = default;
+
+    PGMimageProcessor(const PGMimageProcessor& other);
+    PGMimageProcessor& operator=(const PGMimageProcessor& other);
+    PGMimageProcessor(PGMimageProcessor&& other) noexcept;
+    PGMimageProcessor& operator=(PGMimageProcessor&& other) noexcept;
+
+    int extractComponents(unsigned char threshold, int minValidSize);
+    int filterComponentsBySize(int minSize, int maxSize);
+    bool writeComponents(const std::string& outFileName) const;
+    int getComponentCount() const;
+    int getLargestSize() const;
+    int getSmallestSize() const;
+    void printComponentData(const ConnectedComponent& theComponent) const;
+    const std::vector<std::unique_ptr<ConnectedComponent>>& getComponents() const;
+};
+
+#endif
